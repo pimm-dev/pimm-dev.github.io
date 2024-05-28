bc7776c1e8068a47ac25ebcec0eb872550b26234
Park, Jonghyeon <belline0124@gmail.com>
2024-05-28 18:42:26 +0900

    Update Careers.svelte

--- src/lib/includes/Careers/Careers.svelte
+++ src/lib/includes/Careers/Careers.svelte
@@ -6,39 +6,41 @@
 <BodySection id='careers'>
   <h2>그동안 피워낸 것들</h2>
   <CareersSection title='2024'>
+    <li><a href="https://www.sojoong.kr/www/notice/view/565" target="_blank">&lt;2024 전남대학교 제 6회 SW프로그래밍 경진대회&gt; 대상, 금상, 동상</a></li>
+    <li><a href="https://www.acmicpc.net/category/detail/4225" target="_blank">&lt;2024 GIST 알고리즘 마스터즈&gt;</a> 금상, 동상, 장려상</li>
     <li>백준 온라인 저지 오픈 컨테스트 <a href="https://www.acmicpc.net/contest/view/1247" target="_blank">&lt;2024 상반기 전남대학교 PIMM 알고리즘 파티&gt;</a> 개최</li>
   </CareersSection>
   <CareersSection title='2023'>
     <li>백준 온라인 저지 오픈 컨테스트 <a href="https://www.acmicpc.net/contest/view/1095" target="_blank">&lt;2023 전남대학교 PIMM 알고리즘 파티&gt;</a> 개최</li>
     <li>2023 ICPC Seoul Regional 진출</li>
     <li>SW 동아리 발굴 및 지원프로그램 최종결과발표회 은상</li>
-    <li>제5회 SW프로그래밍 경진대회 은상</li>
+    <li>&lt;2023 전남대학교 제 5회 SW프로그래밍 경진대회&gt; 은상</li>
   </CareersSection>
   <CareersSection title='2022'>
-    <li>광주글로벌게임센터 인디스타즈 2022 우수상</li>
+    <li>&lt;광주글로벌게임센터 인디스타즈 2022&gt; 우수상</li>
     <li>2022 ICPC Seoul Regional 진출</li>
     <li>&lt;태그헌터&gt; 팀, 기업 &lt;레인디어게임즈&gt; 설립</li>
-    <li>제 3회 Sw프로그래밍 경진대회 금상, 은상, 장려상</li>
+    <li>&lt;2022 전남대학교 제 3회 SW프로그래밍 경진대회&gt; 금상, 은상, 장려상</li>
   </CareersSection>
   <CareersSection title='2021'>
     <li><a href="https://www.gicon.or.kr/gallery.es?mid=a10302000000&bid=0001&list_no=370&act=view" target="_blank">광주글로벌게임센터 인디스타즈 2021 우수상</a></li>
     <li><a href="https://store.steampowered.com/app/1147560/Skul_The_Hero_Slayer/?l=koreana" target="_blank">&lt;스컬: 더 히어로 슬레이어&gt;</a> 출시</li>
     <li>2021 ICPC Seoul Regional 진출</li>
-    <li>2021년 제 1회 전남대학교 소프트웨어 프로그래밍 경진대회 입상</li>
+    <li>&lt;2021 제 1회 전남대학교 소프트웨어 프로그래밍 경진대회&gt; 입상</li>
   </CareersSection>
   <CareersSection title='2020'>
-    <li>광주글로벌게임센터 인디스타즈 2020 대상, 우수상</li>
+    <li>&lt;광주글로벌게임센터 인디스타즈 2020&gt; 대상, 우수상</li>
     <li>itch.io GameOff 2020 Submission Release: <a href="https://kwon770.itch.io/moon-slasher" target="_blank">&lt;MoonSlasher&gt;</a></li>
   </CareersSection>
   <CareersSection title='2019'>
-    <li>광주글로벌게임센터 인디스타즈 2019 대상</li>
-    <li>2019 콘텐츠아카데미 여름방학과정 우수상</li>
+    <li>&lt;광주글로벌게임센터 인디스타즈 2019&gt; 대상</li>
+    <li>&lt;2019 콘텐츠아카데미 여름방학과정&gt; 우수상</li>
     <li>&lt;농사대마왕&gt; 출시</li>
     <li>&lt;WarGround&gt; 출시</li>
-    <li>제 6회 공학교육혁신아이디어공모전 가작</li>
+    <li>&lt;제 6회 공학교육혁신아이디어공모전&gt; 가작</li>
   </CareersSection>
   <CareersSection title='2018'>
-    <li>광주글로벌게임센터 인디스타즈 2018 최우수상</li>
+    <li>&lt;광주글로벌게임센터 인디스타즈 2018&gt; 최우수상</li>
     <li>&lt;스컬: 더 히어로 슬레이어&gt; 팀, 기업 <a href="https://southpaw.games/" target="_blank">&lt;사우스포게임즈(주)&gt;</a> 설립</li>
     <li>&lt;BoxCat&gt; 출시</li>
   </CareersSection>
@@ -47,9 +49,9 @@
     <li>&lt;Xmas Box&gt; 출시</li>
   </CareersSection>
   <CareersSection title='2016'>
-    <li>잡(JOB) 로드맵 경진대회 금상</li>
+    <li>&lt;잡(JOB) 로드맵 경진대회&gt; 금상</li>
   </CareersSection>
   <CareersSection title='2012'>
-    <li>산학협력 선도대학(LINC) 육성사업단 제1회 아이디어 및 작품 경진 대회 우수상</li>
+    <li>&lt;산학협력 선도대학(LINC) 육성사업단 제1회 아이디어 및 작품 경진 대회&gt; 우수상</li>
   </CareersSection>
 </BodySection>
