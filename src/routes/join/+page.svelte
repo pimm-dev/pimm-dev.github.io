937d80a1a5ff446dc42ff451e3647b8d30a22952
Jonghyeon Park <belline0124@gmail.com>
2024-08-08 09:33:13 +0900

    Update +page.svelte

--- src/routes/join/+page.svelte
+++ src/routes/join/+page.svelte
@@ -2,6 +2,6 @@
   import { browser } from '$app/environment'
 
   if (browser) {
-    window.location.href = 'https://forms.gle/HkADo5YbCm4xvZKB8'
+    window.location.href = 'https://forms.gle/gi5rhLvANA6JPgM48'
   }
 </script>
