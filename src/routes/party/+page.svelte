5435184c5a4e23cb40b3acfebec5d08cc893ae44
Jonghyeon Park <belline0124@gmail.com>
2024-09-04 14:27:04 +0900

    Update +page.svelte

--- src/routes/party/+page.svelte
+++ src/routes/party/+page.svelte
@@ -2,6 +2,6 @@
   import { browser } from '$app/environment'
 
   if (browser) {
-    window.location.href = 'https://www.acmicpc.net/contest/view/1247'
+    window.location.href = 'https://www.acmicpc.net/contest/view/1357'
   }
 </script>
