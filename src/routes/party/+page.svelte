b3076f85c2f1e03cbde7999560d292b78ab84e73
Jonghyeon Park <belline0124@gmail.com>
2025-02-06 23:14:19 +0900

    Update +page.svelte

--- src/routes/party/+page.svelte
+++ src/routes/party/+page.svelte
@@ -1,6 +1,11 @@
 <iframe src="https://shapelayer.notion.site/ebd/2025-PIMM-1927ad1110f680528ceed26ebb4ab425?pvs=73" frameborder="0" allowfullscreen />
 <style>
   iframe {
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
     width: 100%;
     height: 100%;
   }
