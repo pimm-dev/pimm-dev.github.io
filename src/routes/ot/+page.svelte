6e6dcac59379f4ade5e5d338b734f60e10b400a3
Jonghyeon Park <belline0124@gmail.com>
2025-02-06 23:38:05 +0900

    Create +page.svelte

---
+++ src/routes/ot/+page.svelte
@@ -0,0 +1,12 @@
+<iframe src="https://embed.figma.com/proto/J9xsFliFWOz68FD4Pimfga/PIMM-'25-OT?node-id=71-2&p=f&scaling=contain&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=71%3A2&embed-host=share" allowfullscreen frameborder="0"></iframe>
+<style>
+  iframe {
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+  }
+</style>
